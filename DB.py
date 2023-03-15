e357676fbfe7b6d173ab84c9f745d08da53d6114
aaguasca <arnau.aguasca@fqa.ub.edu>
2023-03-15 14:43:02 +0100

    add database and alumni

---
+++ DB.py
@@ -0,0 +1,6 @@
+class DB:
+    def __init__(self, name,address):
+        self.name=name
+        self.address=address
+    def __str__(self):
+        return "Name:{}; Address:{}".format(self.name, self.address)
---
+++ alumni.py
@@ -0,0 +1,4 @@
+from DB import DB
+
+db=DB(name="Arnau", address="arnau.aguasca@fqa.ub.edu")
+print(db)
